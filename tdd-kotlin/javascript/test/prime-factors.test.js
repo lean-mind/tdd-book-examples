76eb9da519426a84b046819c0648de39570ba8a5
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-13 23:57:21 +0100

    ❌ getPrimeFactorsFor

--- tdd-kotlin/javascript/test/prime-factors.test.js
+++ tdd-kotlin/javascript/test/prime-factors.test.js
@@ -5,4 +5,5 @@ it("finds the prime composition of the given number", () => {
     expect(getPrimeFactorsFor(2 * 2)).toEqual([2, 2]);
     expect(getPrimeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
     expect(getPrimeFactorsFor(3)).toEqual([3]);
+    expect(getPrimeFactorsFor(5 * 5)).toEqual([5, 5]);
 });
