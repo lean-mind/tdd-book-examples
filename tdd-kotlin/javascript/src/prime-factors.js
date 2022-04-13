a0c6dcec002a0f75e9885553b44a91d76d81a20d
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-14 00:01:25 +0100

    ♻ getPrimeFactorsFor code

--- tdd-kotlin/javascript/src/prime-factors.js
+++ tdd-kotlin/javascript/src/prime-factors.js
@@ -3,12 +3,11 @@ function getPrimeFactorsFor(number) {
     while (number % factor != 0) {
         ++factor;
     }
-    let factors = [factor];
     let remainder = number / factor;
     if (remainder > 1) {
-        factors = factors.concat(getPrimeFactorsFor(remainder));
+        return [factor].concat(getPrimeFactorsFor(remainder));
     }
-    return factors;
+    return [factor];
 }
 
 export { getPrimeFactorsFor };
