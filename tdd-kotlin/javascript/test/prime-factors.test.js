5c4274b3a7b131034300b9fbea238c895307828e
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-13 23:59:38 +0100

    ♻ getPrimeFactorsFor

--- tdd-kotlin/javascript/test/prime-factors.test.js
+++ tdd-kotlin/javascript/test/prime-factors.test.js
@@ -1,10 +1,14 @@
 import { getPrimeFactorsFor } from "../src/prime-factors";
 
-it("finds the prime composition of the given number", () => {
+it("knows what is a primer number", () => {
     expect(getPrimeFactorsFor(2)).toEqual([2]);
-    expect(getPrimeFactorsFor(2 * 2)).toEqual([2, 2]);
-    expect(getPrimeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
     expect(getPrimeFactorsFor(3)).toEqual([3]);
-    expect(getPrimeFactorsFor(5 * 5)).toEqual([5, 5]);
+});
+
+it("produces the same result to multiply the numbers in the output list", () => {
+    expect(getPrimeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
+});
+
+it("orders the prime factors from the smallest to the biggest", () => {
     expect(getPrimeFactorsFor(5 * 7 * 11 * 3)).toEqual([3, 5, 7, 11]);
 });
