ff162ea61d1e7446228785e6956ed914dc5f4c03
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-14 08:54:27 +0100

    ✅ getPrimeFactorsFor infinite loop

--- tdd-kotlin/javascript/src/prime-factors.js
+++ tdd-kotlin/javascript/src/prime-factors.js
@@ -1,9 +1,10 @@
 function getPrimeFactorsFor(number) {
+    if (number < 2) return [number];
+
     let prime = findSmallestPrime(number);
     let remainder = number / prime;
-    if (remainder <= 1) {
-        return [prime];
-    }
+    if (remainder <= 1) return [prime];
+
     return [prime].concat(getPrimeFactorsFor(remainder));
 }
 
