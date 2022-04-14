73f3fe1ff1ac4625f78cc85b0d18e395a1f427f4
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-14 17:36:25 +0100

    🔥Removed example

--- tdd-kotlin/python/src/narcissistic_number.py
+++
@@ -1,4 +0,0 @@
-def is_narcissistic(given_number: int) -> bool:
-    digits = [int(n) for n in str(given_number)]
-    calculated_digits = list(map(lambda d: pow(d, len(digits)), digits))
-    return sum(calculated_digits) == given_number
