aa0a11eb25e446ba8acfa9f883998b7c7db7ae93
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-14 18:00:50 +0100

    ✅test_hash_has_always_the_same_fixed_length

--- tdd-kotlin/python/src/hash_text.py
+++ tdd-kotlin/python/src/hash_text.py
@@ -5,4 +5,5 @@ def hash_given(text: str) -> str:
     if len(text) < 10:
         hash = text + str(random.random())
         return hash[0:10]
-    return text
+    else:
+        return text[0:10]
