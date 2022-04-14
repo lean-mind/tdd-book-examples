07a91f5da7e565876ffe3e56fcce28931883c44d
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-14 17:45:59 +0100

    ❌test_hash_has_always_the_same_fixed_length

--- tdd-kotlin/python/test/test_hash_text.py
+++ tdd-kotlin/python/test/test_hash_text.py
@@ -13,3 +13,8 @@ def test_hash_is_always_the_same_given_the_same_input(text):
 def test_hash_is_different_for_each_input(text1, text2):
     assume(text1 != text2)
     assert hash_given(text1) != hash_given(text2)
+
+
+@given(text())
+def test_hash_has_always_the_same_fixed_length(text):
+    assert len(hash(text)) == 10
