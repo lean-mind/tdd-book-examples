c08b4af8828663e58b65bd824689a0cee7cb75b9
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-15 10:09:57 +0100

    ❌exclude_lines_with_both_tax_fields_populated_even_if_non_decimal

--- tdd-kotlin/kotlin/src/test/kotlin/examples/CsvFilterShould.kt
+++ tdd-kotlin/kotlin/src/test/kotlin/examples/CsvFilterShould.kt
@@ -41,4 +41,13 @@ class CsvFilterShould {
 
         assertThat(result).isEqualTo(listOf(headerLine))
     }
+
+    @Test
+    fun exclude_lines_with_both_tax_fields_populated_even_if_non_decimal() {
+        val invoiceLine = "1,02/05/2019,1000,810,XYZ,12,ACER Laptop,B76430134,"
+
+        val result = CsvFilter().filter(listOf(headerLine, invoiceLine))
+
+        assertThat(result).isEqualTo(listOf(headerLine))
+    }
 }
