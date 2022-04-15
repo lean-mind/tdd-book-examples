3faaf0bbf59cb93c892465adb3aec583f08a0113
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-15 09:12:27 +0100

    🧱Setup kotlin example structure

---
+++ tdd-kotlin/kotlin/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin("jvm") version "1.6.10"
+  id("com.diffplug.spotless") version "6.1.2"
+  application
+}
+
+group = "es.leanmind"
+
+version = "0.0.1-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories { mavenCentral() }
+
+dependencies {
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xjsr305=strict")
+    jvmTarget = "17"
+  }
+}
+
+tasks.withType<Test> { useJUnitPlatform() }
+
+spotless {
+  kotlin { ktfmt() }
+  kotlinGradle { ktfmt() }
+}
+
+tasks.check { dependsOn(tasks.spotlessCheck) }
