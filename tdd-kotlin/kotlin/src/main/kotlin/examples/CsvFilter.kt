e16eec7745f9b51ae03059305cba95c2ca1fdc4f
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-15 10:00:40 +0100

    ✅exclude_lines_with_both_tax_fields_empty_as_one_is_required

--- tdd-kotlin/kotlin/src/main/kotlin/examples/CsvFilter.kt
+++ tdd-kotlin/kotlin/src/main/kotlin/examples/CsvFilter.kt
@@ -1,12 +1,14 @@
 package examples
 
 class CsvFilter {
-    fun filter(lines: List<String>) : List<String> {
+    fun filter(lines: List<String>): List<String> {
         val result = mutableListOf<String>()
         result.add(lines[0])
         val invoice = lines[1]
         val fields = invoice.split(',')
-        if (fields[4].isEmpty() || fields[5].isEmpty()){
+        if ((fields[4].isEmpty() || fields[5].isEmpty()) &&
+            (!(fields[4].isEmpty() && fields[5].isEmpty()))
+        ) {
             result.add(lines[1])
         }
         return result.toList()
