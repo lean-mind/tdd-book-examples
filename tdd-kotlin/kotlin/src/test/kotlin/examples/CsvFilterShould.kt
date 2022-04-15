3691eb85917ba49b644fe07ddfd2af9b41c5b626
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-15 10:06:23 +0100

    ❌exclude_lines_with_non_decimal_tax_fields

--- tdd-kotlin/kotlin/src/test/kotlin/examples/CsvFilterShould.kt
+++ tdd-kotlin/kotlin/src/test/kotlin/examples/CsvFilterShould.kt
@@ -4,9 +4,10 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CsvFilterShould {
+    val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+
     @Test
     fun allow_for_correct_lines_only() {
-        val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
         val invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,"
 
         val result = CsvFilter().filter(listOf(headerLine, invoiceLine))
@@ -16,7 +17,6 @@ class CsvFilterShould {
 
     @Test
     fun exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive() {
-        val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
         val invoiceLine = "1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,"
 
         val result = CsvFilter().filter(listOf(headerLine, invoiceLine))
@@ -26,8 +26,16 @@ class CsvFilterShould {
 
     @Test
     fun exclude_lines_with_both_tax_fields_empty_as_one_is_required() {
-        val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
         val invoiceLine = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,"
+
+        val result = CsvFilter().filter(listOf(headerLine, invoiceLine))
+
+        assertThat(result).isEqualTo(listOf(headerLine))
+    }
+
+    @Test
+    fun exclude_lines_with_non_decimal_tax_fields() {
+        val invoiceLine = "1,02/05/2019,1000,810,XYZ,,ACER Laptop,B76430134,"
 
         val result = CsvFilter().filter(listOf(headerLine, invoiceLine))
 
