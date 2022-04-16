320519450cb622f2a64ed2f970edee501c79e2f8
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-16 09:57:08 +0100

    ✨Added ListMatchers code and tests

---
+++ tdd-kotlin/kotlin/src/test/kotlin/examples/ListMatchers.kt
@@ -0,0 +1,31 @@
+package examples
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.AssertionError
+
+fun assertThatList(list: List<Number>): ListMatchers {
+    return ListMatchers(list)
+}
+
+class ListMatchers(private val givenList: List<Number>) {
+
+    fun isExactly(vararg numbers: Number) {
+        assertThat(numbers).hasSize(givenList.size).hasSameElementsAs(givenList)
+    }
+}
+
+class ListMatchersShould {
+
+    @Test
+    fun compare_same_elements_in_both_lists() {
+        assertThatList(listOf(1, 2)).isExactly(1, 2)
+
+        assertThrows<AssertionError> {
+            assertThatList(listOf(1, 2)).isExactly(1, 3)
+        }
+    }
+
+}
+
