a499133dd1c781e82e0ab4019f6c48fb329ed62e
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-26 19:54:18 +0100

    ✅ converts the first character of each word to uppercase

--- tdd-kotlin/javascript/src/to-camel-case.js
+++ tdd-kotlin/javascript/src/to-camel-case.js
@@ -1,6 +1,8 @@
 function toCamelCase(text) {
     const words = text.split(/[ ,_-]/g);
+    let word = words[0];
+    word = word.charAt(0).toUpperCase() + word.substr(1);
+    words[0] = word;
     return words.join("");
 }
-
 export { toCamelCase };
--- tdd-kotlin/javascript/test/to-camel-case.test.js
+++ tdd-kotlin/javascript/test/to-camel-case.test.js
@@ -1,3 +1,5 @@
+const { toCamelCase } = require("../src/to-camel-case");
+
 it("converts the first character of each word to uppercase", () => {
     expect(toCamelCase("foo")).toBe("Foo");
 });
