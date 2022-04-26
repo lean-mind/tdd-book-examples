49d87804567e316e6838a4efc9d2f9011c7fb083
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-26 20:05:09 +0100

    ❌fails when the password is missing a number

--- tdd-kotlin/javascript/test/password-validator.test.js
+++ tdd-kotlin/javascript/test/password-validator.test.js
@@ -7,4 +7,7 @@ describe("The password strength validator", () => {
     it("fails when the password is too short", () => {
         expect(isStrongPassword("1aA_")).toBe(false);
     });
+    it("fails when the password is missing a number", () => {
+        expect(isStrongPassword("abcdABCD_")).toBe(false);
+      });
 });
