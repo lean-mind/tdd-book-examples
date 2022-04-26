68fd6cca2ec4e3498783b0e83641de1adecc953d
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-26 20:02:27 +0100

    ❌ considers a password to be strong when all requirements are met

---
+++ tdd-kotlin/javascript/test/password-validator.test.js
@@ -0,0 +1,5 @@
+describe("The password strength validator", () => {
+    it("considers a password to be strong when all requirements are met", () => {
+        expect(isStrongPassword("1234abcdABCD_")).toBe(true);
+    });
+});
