a00dddec5d1e0897735759ab1a894a2466dbab25
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-26 19:55:07 +0100

    ♻ to camel case

--- tdd-kotlin/javascript/src/to-camel-case.js
+++ tdd-kotlin/javascript/src/to-camel-case.js
@@ -1,8 +1,9 @@
 function toCamelCase(text) {
-    const words = text.split(/[ ,_-]/g);
-    let word = words[0];
-    word = word.charAt(0).toUpperCase() + word.substr(1);
-    words[0] = word;
-    return words.join("");
+    return text
+        .split(/[ ,_-]/g)
+        .map((word) => {
+            return word.charAt(0).toUpperCase() + word.substr(1);
+        })
+        .join("");
 }
 export { toCamelCase };
