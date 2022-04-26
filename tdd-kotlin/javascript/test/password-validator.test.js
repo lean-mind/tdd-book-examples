3e4dfb62e6dd8b3369188f2aacf560fc2c027dd9
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-26 20:04:08 +0100

    ❌ fails when the password is too short

--- tdd-kotlin/javascript/test/password-validator.test.js
+++ tdd-kotlin/javascript/test/password-validator.test.js
@@ -4,4 +4,7 @@ describe("The password strength validator", () => {
     it("considers a password to be strong when all requirements are met", () => {
         expect(isStrongPassword("1234abcdABCD_")).toBe(true);
     });
+    it("fails when the password is too short", () => {
+        expect(isStrongPassword("1aA_")).toBe(false);
+    });
 });
