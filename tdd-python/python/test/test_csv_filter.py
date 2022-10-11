2a773961d521acd37c53a18ebc9db7116b790578
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-10-11 18:51:07 +0100

    ♻️ CsvFilterShould extract headerline

--- tdd-python/python/test/test_csv_filter.py
+++ tdd-python/python/test/test_csv_filter.py
@@ -4,26 +4,28 @@ from src.csv_filter import CsvFilter
 
 
 class CsvFilterShould(TestCase):
+    
+    def __init__(self, methodName: str = ...) -> None:
+        self.headerLine =  "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        super().__init__(methodName)
+    
     def test_allow_for_correct_lines_only(self):
-        headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
         invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,"
 
-        result = CsvFilter().filter([headerLine, invoiceLine])
+        result = CsvFilter().filter([self.headerLine, invoiceLine])
 
-        assert_that(result).is_equal_to([headerLine, invoiceLine])
+        assert_that(result).is_equal_to([self.headerLine, invoiceLine])
 
     def test_exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive(self):
-        headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
         invoiceLine = "1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,"
 
-        result = CsvFilter().filter([headerLine, invoiceLine])
+        result = CsvFilter().filter([self.headerLine, invoiceLine])
 
-        assert_that(result).is_equal_to([headerLine])
+        assert_that(result).is_equal_to([self.headerLine])
 
     def test_exclude_lines_with_both_tax_fields_empty_as_one_is_required(self):
-        headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
         invoiceLine = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,"
 
-        result = CsvFilter().filter([headerLine, invoiceLine])
+        result = CsvFilter().filter([self.headerLine, invoiceLine])
 
-        assert_that(result).is_equal_to([headerLine])
+        assert_that(result).is_equal_to([self.headerLine])
