273a284ed65326bcb009550043c3ffa34b04f902
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-10-11 18:36:54 +0100

    ✅ test_exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive

--- tdd-python/python/src/csv_filter.py
+++ tdd-python/python/src/csv_filter.py
@@ -4,4 +4,11 @@ from typing import List
 class CsvFilter:
 
     def filter(self, lines: List[str]) -> List[str]:
-        return lines
+        result = []
+        result.append(lines[0])
+        invoice = lines[1]
+        fields = [field for field in invoice.split(',')]
+        if not fields[4] or not fields[5]:
+            result.append(invoice)
+        return result
+
