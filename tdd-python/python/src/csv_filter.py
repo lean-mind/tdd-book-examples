345ad6b0f7f12fe992f15a9df386e255ded9ced7
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-10-11 19:09:17 +0100

    ✅ test_exclude_lines_with_non_decimal_tax_fields

--- tdd-python/python/src/csv_filter.py
+++ tdd-python/python/src/csv_filter.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 
 
@@ -7,11 +8,16 @@ class CsvFilter:
         result = []
         result.append(lines[0])
         invoice = lines[1]
+        fields = [field for field in invoice.split(',')]
         ivaFieldIndex = 4
         igicFieldIndex = 5
-        fields = [field for field in invoice.split(',')]
-        if (not fields[ivaFieldIndex] or not fields[igicFieldIndex])\
-                and not (not fields[ivaFieldIndex]
-                         and not fields[igicFieldIndex]):
+        ivaField = fields[ivaFieldIndex]
+        igicField = fields[igicFieldIndex]
+        decimalRegex = re.compile("\\d+(\\.\\d+)?")
+        taxFieldsAreMutuallyExclusive = (decimalRegex.match(ivaField)
+                                         or decimalRegex.match(igicField))\
+            and not (decimalRegex.match(ivaField)
+                     and decimalRegex.match(igicField))
+        if taxFieldsAreMutuallyExclusive:
             result.append(invoice)
         return result
