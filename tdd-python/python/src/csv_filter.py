f9639dc1515bd4b17d32ae088fab21cd33aefc73
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-10-11 18:25:47 +0100

    ❌ test_allow_for_correct_lines_only

---
+++ tdd-python/python/src/csv_filter.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class CsvFilter:
+
+    def filter(self, lines: List[str]) -> List[str]:
+        return []
---
+++ tdd-python/python/test/test_csv_filter.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from assertpy import assert_that
+from src.csv_filter import CsvFilter
+
+
+class CsvFilterShould(TestCase):
+    def test_allow_for_correct_lines_only(self):
+        headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,"
+
+        result = CsvFilter().filter([headerLine, invoiceLine])
+
+        assert_that(result).is_equal_to([headerLine, invoiceLine])
