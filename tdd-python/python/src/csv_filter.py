6e2da63c1b46119225df2e7584e3a480af3743b5
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-10-24 21:03:07 +0100

    🚚 rename to snake case

--- tdd-python/python/src/csv_filter.py
+++ tdd-python/python/src/csv_filter.py
@@ -9,15 +9,15 @@ class CsvFilter:
         result.append(lines[0])
         invoice = lines[1]
         fields = [field for field in invoice.split(',')]
-        ivaFieldIndex = 4
-        igicFieldIndex = 5
-        ivaField = fields[ivaFieldIndex]
-        igicField = fields[igicFieldIndex]
-        decimalRegex = re.compile("\\d+(\\.\\d+)?")
-        taxFieldsAreMutuallyExclusive = (
-            decimalRegex.match(ivaField)
-            or decimalRegex.match(igicField)
-        ) and (not ivaField or not igicField)
-        if taxFieldsAreMutuallyExclusive:
+        iva_field_index = 4
+        igic_field_index = 5
+        iva_field = fields[iva_field_index]
+        igic_field = fields[igic_field_index]
+        decimal_regex = re.compile("\\d+(\\.\\d+)?")
+        tax_fields_are_mutually_exclusive = (
+            decimal_regex.match(iva_field)
+            or decimal_regex.match(igic_field)
+        ) and (not iva_field or not igic_field)
+        if tax_fields_are_mutually_exclusive:
             result.append(invoice)
         return result
--- tdd-python/python/test/test_csv_filter.py
+++ tdd-python/python/test/test_csv_filter.py
@@ -6,40 +6,40 @@ from src.csv_filter import CsvFilter
 class CsvFilterShould(TestCase):
     
     def __init__(self, methodName: str = ...) -> None:
-        self.headerLine =  "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        self.header_line =  "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
         super().__init__(methodName)
     
     def test_allow_for_correct_lines_only(self):
-        invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,"
+        invoice_line = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,"
 
-        result = CsvFilter().filter([self.headerLine, invoiceLine])
+        result = CsvFilter().filter([self.header_line, invoice_line])
 
-        assert_that(result).is_equal_to([self.headerLine, invoiceLine])
+        assert_that(result).is_equal_to([self.header_line, invoice_line])
 
     def test_exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive(self):
-        invoiceLine = "1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,"
+        invoice_line = "1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134,"
 
-        result = CsvFilter().filter([self.headerLine, invoiceLine])
+        result = CsvFilter().filter([self.header_line, invoice_line])
 
-        assert_that(result).is_equal_to([self.headerLine])
+        assert_that(result).is_equal_to([self.header_line])
 
     def test_exclude_lines_with_both_tax_fields_empty_as_one_is_required(self):
-        invoiceLine = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,"
+        invoice_line = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,"
 
-        result = CsvFilter().filter([self.headerLine, invoiceLine])
+        result = CsvFilter().filter([self.header_line, invoice_line])
 
-        assert_that(result).is_equal_to([self.headerLine])
+        assert_that(result).is_equal_to([self.header_line])
 
     def test_exclude_lines_with_non_decimal_tax_fields(self):
-        invoiceLine = "1,02/05/2019,1000,810,XYZ,,ACER Laptop,B76430134,"
+        invoice_line = "1,02/05/2019,1000,810,XYZ,,ACER Laptop,B76430134,"
         
-        result = CsvFilter().filter([self.headerLine, invoiceLine])
+        result = CsvFilter().filter([self.header_line, invoice_line])
         
-        assert_that(result).is_equal_to([self.headerLine])
+        assert_that(result).is_equal_to([self.header_line])
         
     def test_exclude_lines_with_both_tax_fields_populated_even_if_non_decimal(self):
-        invoiceLine = "1,02/05/2019,1000,810,XYZ,12,ACER Laptop,B76430134,"
+        invoice_line = "1,02/05/2019,1000,810,XYZ,12,ACER Laptop,B76430134,"
         
-        result = CsvFilter().filter([self.headerLine, invoiceLine])
+        result = CsvFilter().filter([self.header_line, invoice_line])
         
-        assert_that(result).is_equal_to([self.headerLine])
+        assert_that(result).is_equal_to([self.header_line])
