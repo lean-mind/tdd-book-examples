176517cb87d3b1402287dbccf65dcc39d8b1d6d3
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-11-14 13:33:06 +0000

    🚚 rename test for test and install behave

--- tdd-python/python/tests/test_diagnoses_example.py
+++ tdd-python/python/tests/test_diagnoses_example.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from assertpy import assert_that
-from test.fixtures.diagnoses_builder import CasesWithDiagnosesBuilder
+from tests.fixtures.diagnoses_builder import CasesWithDiagnosesBuilder
 
 
 class TestDiagnosesExample(TestCase):
