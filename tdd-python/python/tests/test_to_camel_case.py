5f51bcc304b24d67ff3accb23517bd179f95405f
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-11-17 12:08:39 +0000

    ♻️ generalize the code

--- tdd-python/python/tests/test_to_camel_case.py
+++ tdd-python/python/tests/test_to_camel_case.py
@@ -1,11 +1,13 @@
+import re
 from assertpy import assert_that
 
+
 def to_camel_case(text: str) -> str:
-    words = text.split(r"[ ,_-]")
-    word = words[0]
-    word = word[0].upper() + word[1:]
-    words[0] = word
+    words = re.split(r"[ ,_-]", text)
+    words = [w[0].upper() + w[1:] for w in words]
     return "".join(words)
+
 
 def test_converts_the_first_character_of_each_word_to_uppercase():
     assert_that(to_camel_case("foo")).is_equal_to("Foo")
+    assert_that(to_camel_case("foo bar")).is_equal_to("FooBar")
