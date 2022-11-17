8a6375cf44cc114c24b644eaa89005381dfd73cc
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-11-17 10:31:17 +0000

    ♻️ introduce explaining variable

--- tdd-python/python/tests/test_get_prime_factors.py
+++ tdd-python/python/tests/test_get_prime_factors.py
@@ -3,11 +3,16 @@ from assertpy import assert_that
 
 
 def get_prime_factors_for(number: int) -> list[int]:
-    if number == 2:
-        return [2]
+    factor = 2
+    factors = [factor]
+    reminder = number / factor
+    if reminder > 1:
+        factors.append(2)
+    return factors
 
 
 class TestGetPrimeFactors(TestCase):
 
     def test_finds_the_prime_composition_of_the_given_number(self):
         assert_that(get_prime_factors_for(2)).is_equal_to([2])
+        assert_that(get_prime_factors_for(2 * 2)).is_equal_to([2, 2])
