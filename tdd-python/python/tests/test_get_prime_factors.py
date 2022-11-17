f37138f71f13a9957998800304e49d9e70e384f0
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-11-17 11:04:14 +0000

    ♻️ split in various unit tests

--- tdd-python/python/tests/test_get_prime_factors.py
+++ tdd-python/python/tests/test_get_prime_factors.py
@@ -3,13 +3,23 @@ from assertpy import assert_that
 
 def get_prime_factors_for(number: int) -> list[int]:
     factor = 2
+    while number % factor != 0:
+        factor += 1
     factors = [factor]
     reminder = number / factor
     if reminder > 1:
-        factors.append(2)
+        factors += get_prime_factors_for(reminder)
     return factors
 
 
-def test_finds_the_prime_composition_of_the_given_number():
+def test_knows_what_is_a_primer_number():
     assert_that(get_prime_factors_for(2)).is_equal_to([2])
-    assert_that(get_prime_factors_for(2 * 2)).is_equal_to([2, 2])
+    assert_that(get_prime_factors_for(3)).is_equal_to([3])
+
+
+def test_produces_the_same_result_to_multiply_the_numbers_in_the_output_list():
+    assert_that(get_prime_factors_for(2 * 2 * 2)).is_equal_to([2, 2, 2])
+
+
+def test_orders_the_prime_factors_from_the_smallest_to_the_biggest():
+    assert_that(get_prime_factors_for(5 * 7 * 11 * 3)).is_equal_to([3, 5, 7, 11])
