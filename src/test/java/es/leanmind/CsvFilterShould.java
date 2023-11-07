6258bf71454b04ce8755e22b0b0deadbc5994cf6
Wolfremium13 <kevin@leanmind.es>
2023-11-07 19:49:06 +0000

    🔥 remove public from test

--- src/test/java/es/leanmind/CsvFilterShould.java
+++ src/test/java/es/leanmind/CsvFilterShould.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class CsvFilterShould {
 
     @Test
-    public void test_allow_for_correct_lines_only() {
+    void test_allow_for_correct_lines_only() {
         var headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
         var invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,";
         var incorrectLine = "A,line,missing,fields,and,wrong,formats,";
