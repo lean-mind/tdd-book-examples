347c0145eee53f14870d8e338567ae5850a50c39
Wolfremium13 <kevin@leanmind.es>
2023-11-07 19:45:24 +0000

    ❌ test_allow_for_correct_lines_only

---
+++ src/main/java/es/leanmind/Csv.java
@@ -0,0 +1,9 @@
+package es.leanmind;
+
+import java.util.List;
+
+public class Csv {
+    public static List<String> filter(List<String> lines) {
+        return List.of();
+    }
+}
--- src/test/java/es/leanmind/CsvFilterShould.java
+++ src/test/java/es/leanmind/CsvFilterShould.java
@@ -2,12 +2,20 @@ package es.leanmind;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CsvFilterShould {
 
     @Test
-    public void do_something() {
-        assertThat(true).isEqualTo(true);
+    public void test_allow_for_correct_lines_only() {
+        var headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+        var invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,";
+        var incorrectLine = "A,line,missing,fields,and,wrong,formats,";
+
+        List<String> correctLines = Csv.filter(List.of(headerLine, invoiceLine, incorrectLine));
+
+        assertThat(correctLines).isEqualTo(List.of(headerLine, invoiceLine));
     }
 }
