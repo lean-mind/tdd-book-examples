134fe13697c59c103997b7f12324cd1f44d98433
Wolfremium13 <kevin@leanmind.es>
2023-11-07 19:34:40 +0000

    🎉 starting point

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "es.leanmind"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/java/es/leanmind/CsvFilterShould.java
@@ -0,0 +1,13 @@
+package es.leanmind;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CsvFilterShould {
+
+    @Test
+    public void do_something() {
+        assertThat(true).isEqualTo(true);
+    }
+}
