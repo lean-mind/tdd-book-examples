4afc4f72e298492af91f5c2b8f57fd60510631db
Wolfremium13 <kevin@leanmind.es>
2023-11-07 20:27:39 +0000

    ♻ pull up the header variable

--- src/test/java/es/leanmind/CsvFilterShould.java
+++ src/test/java/es/leanmind/CsvFilterShould.java
@@ -7,10 +7,9 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CsvFilterShould {
-
+    private final String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
     @Test
     void allow_for_correct_lines_only() {
-        var headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
         var invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,";
         var incorrectLine = "A,line,missing,fields,and,wrong,formats,";
 
@@ -21,7 +20,6 @@ class CsvFilterShould {
 
     @Test
     void exclude_lines_with_both_tax_fields_populated() {
-        var headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
         var iva = "19";
         var igic = "8";
         var invoiceLine = "1,02/05/2019,1000,810," + iva + "," + igic + ",ACER Laptop,B76430134,";
@@ -33,7 +31,6 @@ class CsvFilterShould {
 
     @Test
     void at_least_one_tax_is_required() {
-        var headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
         var invoiceLine = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,";
 
         var correctLines = Csv.filter(List.of(headerLine, invoiceLine));
