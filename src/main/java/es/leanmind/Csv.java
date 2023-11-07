0ffe7d844ad15e0dd8a7d190a2233a59908e60b6
Wolfremium13 <kevin@leanmind.es>
2023-11-07 20:01:33 +0000

    ✅ exclude_lines_with_both_tax_fields_populated

--- src/main/java/es/leanmind/Csv.java
+++ src/main/java/es/leanmind/Csv.java
@@ -1,9 +1,19 @@
 package es.leanmind;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Csv {
     public static List<String> filter(List<String> lines) {
-        return List.of(lines.get(0), lines.get(1));
+        var result = new ArrayList<String>();
+        result.add(lines.get(0));
+        var invoiceLine = lines.get(1);
+        var fields = invoiceLine.split(",");
+        var iva = fields[4];
+        var igic = fields[5];
+        if (iva.isEmpty() || igic.isEmpty()) {
+            result.add(invoiceLine);
+        }
+        return result;
     }
 }
