38f20e1f40bd8813f8dada9a48dc5b3720908aea
Wolfremium13 <kevin@leanmind.es>
2023-11-15 05:30:53 +0000

    ♻ refactor chapter 3

--- src/test/java/es/leanmind/CsvFilterShould.java
+++ src/test/java/es/leanmind/CsvFilterShould.java
@@ -2,61 +2,92 @@ package es.leanmind;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CsvFilterShould {
 
-    public static final String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+    public final String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+    public final String emptyField = "";
+    public final List<String> emptyFile = List.of(headerLine);
 
     @Test
     void allow_for_correct_lines_only() {
-        var invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,";
+        var ivaTax = "19";
+        var invoiceLines = fileWithOneInvoiceLineHaving(ivaTax, emptyField);
         var incorrectLine = "A,line,missing,fields,and,wrong,formats,";
+        invoiceLines.add(incorrectLine);
 
-        var correctLines = Csv.filter(List.of(headerLine, invoiceLine, incorrectLine));
+        var correctLines = Csv.filter(invoiceLines);
 
-        assertThat(correctLines).isEqualTo(List.of(headerLine, invoiceLine));
+        assertThat(correctLines).isEqualTo(
+                fileWithOneInvoiceLineHaving(ivaTax, emptyField)
+        );
     }
 
     @Test
     void exclude_lines_with_both_tax_fields_populated() {
-        var iva = "19";
-        var igic = "8";
-        var incorrectLine = "1,02/05/2019,1000,810," + iva + "," + igic + ",ACER Laptop,B76430134,";
+        var ivaTax = "19";
+        var igicTax = "8";
+        var invoiceLines = fileWithOneInvoiceLineHaving(ivaTax, igicTax);
 
-        var correctLines = Csv.filter(List.of(headerLine, incorrectLine));
+        var correctLines = Csv.filter(invoiceLines);
 
-        assertThat(correctLines).isEqualTo(List.of(headerLine));
+        assertThat(correctLines).isEqualTo(emptyFile);
     }
 
     @Test
     void at_least_one_tax_is_required() {
-        var incorrectLine = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,";
+        var invoiceLines = fileWithOneInvoiceLineHaving(emptyField, emptyField);
 
-        var correctLines = Csv.filter(List.of(headerLine, incorrectLine));
+        var correctLines = Csv.filter(invoiceLines);
 
-        assertThat(correctLines).isEqualTo(List.of(headerLine));
+        assertThat(correctLines).isEqualTo(emptyFile);
     }
 
     @Test
     void exclude_lines_with_non_decimal_tax_fields() {
         var wrongTaxType = "XYZ";
-        var incorrectLine = "1,02/05/2019,1000,810," + wrongTaxType + ",,ACER Laptop,B76430134,";
+        var invoiceLines = fileWithOneInvoiceLineHaving(wrongTaxType, emptyField);
 
-        var correctLines = Csv.filter(List.of(headerLine, incorrectLine));
+        var correctLines = Csv.filter(invoiceLines);
 
-        assertThat(correctLines).isEqualTo(List.of(headerLine));
+        assertThat(correctLines).isEqualTo(emptyFile);
     }
 
     @Test
     void test_exclude_lines_with_both_taxes_even_with_wrong_format() {
         var wrongIvaType = "XYZ";
-        var incorrectLine = "1,02/05/2019,1000,810," + wrongIvaType + ",12,ACER Laptop,B76430134,";
+        var igicTax = "8";
+        var invoiceLines = fileWithOneInvoiceLineHaving(wrongIvaType, igicTax);
 
-        var correctLines = Csv.filter(List.of(headerLine, incorrectLine));
+        var correctLines = Csv.filter(invoiceLines);
 
-        assertThat(correctLines).isEqualTo(List.of(headerLine));
+        assertThat(correctLines).isEqualTo(emptyFile);
+    }
+
+    private ArrayList<String> fileWithOneInvoiceLineHaving(String ivaTax, String igicTax) {
+        var invoiceId = "1";
+        var invoiceDate = "02/05/2019";
+        var grossAmount = "1000";
+        var netAmount = "810";
+        var concept = "ACER Laptop";
+        var cif = "B76430134";
+        var nif = "";
+        String invoiceLine =
+                String.join(",",
+                        invoiceId,
+                        invoiceDate,
+                        grossAmount,
+                        netAmount,
+                        ivaTax,
+                        igicTax,
+                        concept,
+                        cif,
+                        nif
+                );
+        return new ArrayList<>(List.of(headerLine, invoiceLine));
     }
 }
