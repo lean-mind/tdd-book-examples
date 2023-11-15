d21575b5abdb6c81c2db42f33ecf1e1f6677e9fc
Wolfremium13 <kevin@leanmind.es>
2023-11-15 03:56:21 +0000

    ♻ rename

--- src/test/java/es/leanmind/CsvFilterShould.java
+++ src/test/java/es/leanmind/CsvFilterShould.java
@@ -43,9 +43,9 @@ class CsvFilterShould {
     @Test
     void exclude_lines_with_non_decimal_tax_fields() {
         var wrongTaxType = "XYZ";
-        var invoiceLine = "1,02/05/2019,1000,810," + wrongTaxType + ",,ACER Laptop,B76430134,";
+        var incorrectLine = "1,02/05/2019,1000,810," + wrongTaxType + ",,ACER Laptop,B76430134,";
 
-        var correctLines = Csv.filter(List.of(headerLine, invoiceLine));
+        var correctLines = Csv.filter(List.of(headerLine, incorrectLine));
 
         assertThat(correctLines).isEqualTo(List.of(headerLine));
     }
