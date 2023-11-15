9910830f3e7f135592a50f096c06a8bcf3164f8c
Wolfremium13 <kevin@leanmind.es>
2023-11-15 03:15:43 +0000

    🔥 remove to start again the examples

--- src/main/java/es/leanmind/Csv.java
+++ src/main/java/es/leanmind/Csv.java
@@ -5,20 +5,6 @@ import java.util.List;
 
 public class Csv {
     public static List<String> filter(List<String> lines) {
-        var header = lines.get(0);
-        var correctLines = new ArrayList<String>();
-        correctLines.add(header);
-        var invoiceLine = lines.get(1);
-        var fields = invoiceLine.split(",");
-        var iva = fields[4];
-        var igic = fields[5];
-        var oneTaxIsPopulated = iva.isEmpty() || igic.isEmpty();
-        var bothTaxesAreNotPopulated = !(iva.isEmpty() && igic.isEmpty());
-        var taxIsDecimal = iva.matches("\\d+") || igic.matches("\\d+");
-        var taxFieldsAreMutuallyExclusive = oneTaxIsPopulated && bothTaxesAreNotPopulated && taxIsDecimal;
-        if (taxFieldsAreMutuallyExclusive) {
-            correctLines.add(invoiceLine);
-        }
-        return correctLines;
+        return List.of();
     }
 }
--- src/test/java/es/leanmind/CsvFilterShould.java
+++ src/test/java/es/leanmind/CsvFilterShould.java
@@ -7,45 +7,8 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CsvFilterShould {
-    private final String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
-
     @Test
-    void allow_for_correct_lines_only() {
-        var invoiceLine = "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,";
-        var incorrectLine = "A,line,missing,fields,and,wrong,formats,";
-
-        var correctLines = Csv.filter(List.of(headerLine, invoiceLine, incorrectLine));
-
-        assertThat(correctLines).isEqualTo(List.of(headerLine, invoiceLine));
-    }
-
-    @Test
-    void exclude_lines_with_both_tax_fields_populated() {
-        var iva = "19";
-        var igic = "8";
-        var invoiceLine = "1,02/05/2019,1000,810," + iva + "," + igic + ",ACER Laptop,B76430134,";
-
-        var correctLines = Csv.filter(List.of(headerLine, invoiceLine));
-
-        assertThat(correctLines).isEqualTo(List.of(headerLine));
-    }
-
-    @Test
-    void at_least_one_tax_is_required() {
-        var invoiceLine = "1,02/05/2019,1000,810,,,ACER Laptop,B76430134,";
-
-        var correctLines = Csv.filter(List.of(headerLine, invoiceLine));
-
-        assertThat(correctLines).isEqualTo(List.of(headerLine));
-    }
-
-    @Test
-    void exclude_lines_with_non_decimal_tax_fields() {
-        var wrongTaxType = "XYZ";
-        var invoiceLine = "1,02/05/2019,1000,810," + wrongTaxType + ",,ACER Laptop,B76430134,";
-
-        var correctLines = Csv.filter(List.of(headerLine, invoiceLine));
-
-        assertThat(correctLines).isEqualTo(List.of(headerLine));
+    void foo() {
+        assertThat(Csv.filter(List.of())).isEqualTo(List.of());
     }
 }
