f589df7e6ad9f5b5bb6b3d0da4b67b7064574e26
Wolfremium13 <kevin@leanmind.es>
2023-11-19 17:07:32 +0000

    ❌ finds_the_prime_composition_of_the_given_number

--- src/test/java/es/leanmind/PrimeFactorsShould.java
+++ src/test/java/es/leanmind/PrimeFactorsShould.java
@@ -8,11 +8,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PrimeFactorsShould {
     public static List<Integer> getPrimeFactorsFor(int number) {
-        return null;
+        return List.of(2);
     }
 
     @Test
     void finds_the_prime_composition_of_the_given_number() {
         assertThat(getPrimeFactorsFor(2)).isEqualTo(List.of(2));
+        assertThat(getPrimeFactorsFor(2 * 2)).isEqualTo(List.of(2, 2));
     }
 }
