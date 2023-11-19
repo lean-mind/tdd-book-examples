6ee4ed44c9ac4df2228a7ba079389d895217286a
Wolfremium13 <kevin@leanmind.es>
2023-11-19 17:19:55 +0000

    ♻ split bussines rules

--- src/test/java/es/leanmind/PrimeFactorsShould.java
+++ src/test/java/es/leanmind/PrimeFactorsShould.java
@@ -23,12 +23,18 @@ public class PrimeFactorsShould {
     }
 
     @Test
-    void finds_the_prime_composition_of_the_given_number() {
+    void knows_what_is_a_prime_number() {
         assertThat(getPrimeFactorsFor(2)).isEqualTo(List.of(2));
-        assertThat(getPrimeFactorsFor(2 * 2)).isEqualTo(List.of(2, 2));
-        assertThat(getPrimeFactorsFor(2 * 2 * 2)).isEqualTo(List.of(2, 2, 2));
         assertThat(getPrimeFactorsFor(3)).isEqualTo(List.of(3));
-        assertThat(getPrimeFactorsFor(5 * 5)).isEqualTo(List.of(5, 5));
+    }
+
+    @Test
+    void produces_the_same_result_to_multiply_the_numbers_in_the_output_list() {
+        assertThat(getPrimeFactorsFor(2 * 2 * 2)).isEqualTo(List.of(2, 2, 2));
+    }
+
+    @Test
+    void orders_the_prime_factors_from_the_smallest_to_the_biggest() {
         assertThat(getPrimeFactorsFor(5 * 7 * 11 * 3)).isEqualTo(List.of(3, 5, 7, 11));
     }
 }
