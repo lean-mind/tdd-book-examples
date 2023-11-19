cfa9c5fc54897538acc9bf28eb7817bc3d3fca3b
Wolfremium13 <kevin@leanmind.es>
2023-11-19 12:31:43 +0000

    ✅ had_always_the_same_fixed_length

--- src/test/java/es/leanmind/cryptography/HashShould.java
+++ src/test/java/es/leanmind/cryptography/HashShould.java
@@ -12,7 +12,13 @@ public class HashShould {
 
     // Function under test:
     private static String hash(String text) {
-        return text;
+        var fixedLength = 10;
+        if (text.length() < fixedLength) {
+            var valuesToAdd = Math.max(fixedLength - text.length(), 0);
+            var randomValues = "1234567890".substring(0, valuesToAdd);
+            return text + randomValues;
+        }
+        return text.substring(0, fixedLength);
     }
 
     // Tests:
@@ -26,6 +32,12 @@ public class HashShould {
     public void be_different_for_each_input(@ForAll String text1, @ForAll String text2) {
         Assume.that(!text1.equals(text2));
         assertThat(hash(text1)).isNotEqualTo(hash(text2));
+    }
+
+    @Property
+    public void had_always_the_same_fixed_length(@ForAll String text) {
+        var fixedLength = 10;
+        assertThat(hash(text).length()).isEqualTo(fixedLength);
     }
 
 }
