7b196a4d5f262fdd3bcdb84e24bb5187712c4085
Wolfremium13 <kevin@leanmind.es>
2023-11-19 17:42:05 +0000

    ✅ converts_the_first_character_of_each_word_to_uppercase

--- src/test/java/es/leanmind/CamelCaseShould.java
+++ src/test/java/es/leanmind/CamelCaseShould.java
@@ -7,7 +7,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CamelCaseShould {
     public static String toCamelCase(String text) {
-        String[] words = text.split("/[ ,_-]/g");
+        var words = text.split("/[ ,_-]/g");
+        var word = words[0];
+        word = word.substring(0, 1).toUpperCase() + word.substring(1);
+        words[0] = word;
         return String.join("", words);
     }
 
