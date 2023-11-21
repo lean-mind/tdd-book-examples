01cd5345247d8bdc5d055aa0a8495a5470d6cea3
Wolfremium13 <kevin@leanmind.es>
2023-11-21 19:30:36 +0000

    ♻ mockito stub

--- src/test/java/es/leanmind/mocks/UserServiceShould.java
+++ src/test/java/es/leanmind/mocks/UserServiceShould.java
@@ -1,13 +1,27 @@
 package es.leanmind.mocks;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class UserServiceShould {
+    private Repository repository;
+    private UserService service;
+    private String irrelevantName = "irrelevantName";
+    private User user;
+
+    @BeforeEach
+    void setup() {
+        repository = mock(Repository.class);
+        service = new UserService(repository);
+        user = new User();
+    }
     @Test
     void save_user_through_repository() {
         var userRepository = new UserRepositorySpy();
@@ -21,27 +35,17 @@ public class UserServiceShould {
 
     @Test
     void search_users_by_name() {
-        var repository = new UserRepositoryStub();
-        var service = new UserService(repository);
-        var irrelevantName = "irrelevantName";
-        var user = new User();
-        repository.stubListOfUsersByName = List.of(user);
+        when(repository.findUsersByName(irrelevantName))
+                .thenReturn(List.of(user));
 
-        var users = service.findUsers(irrelevantName);
-
-        assertThat(users).containsOnly(user);
+        assertThat(service.findUsers(irrelevantName)).containsOnly(user);
     }
 
     @Test
     void search_users_by_surname_when_nothing_is_found_by_name() {
-        var repository = new UserRepositoryStub();
-        var service = new UserService(repository);
-        var irrelevantName = "irrelevantName";
-        var user = new User();
-        repository.stubListOfUsersBySurname = List.of(user);
+        when(repository.findUsersBySurname(irrelevantName))
+                .thenReturn(List.of(user));
 
-        var users = service.findUsers(irrelevantName);
-
-        assertThat(users).containsOnly(user);
+        assertThat(service.findUsers(irrelevantName)).containsOnly(user);
     }
 }
